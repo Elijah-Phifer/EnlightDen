fe70ec5fba00af3f88f4bee0af78712c2672ebd9
Luke Norton <luke.norton@selu.edu>
2024-08-31 14:08:34 -0500

    dependencies

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0645da99a661767200fe980521baa29031725b47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0582cf9e65fe0d54ca526e20173af05b442a9f54")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
